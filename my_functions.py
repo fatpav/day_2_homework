179f7ec99be0ec7ab44b1683351bf973d16a4d42
fatpav <fat_pav@hotmail.com>
2020-09-08 20:12:32 +0100

    Added return code and some more notes

--- my_functions.py
+++ my_functions.py
@@ -1,4 +1,14 @@
 # def command 'defines' a function
+# The colon at the end indicates there will be code inside the function
+
+# def greet():
+#      print("Hey there")
+# greet()
+
+# The function is 'called' with the function name and brackets 
+
+# the return command is used when you want something from the function.
+# instead of printing :"Hey there" it will return it
 def greet():
-    print("Hey there")
+    return "Hey there"
 greet()
